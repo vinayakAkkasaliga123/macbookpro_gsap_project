64a48f59ca09bb9ea5c60a5dce78e7fde196f789
vinayakAkkasaliga123 <akkasaligavinayak@gmail.com>
2026-02-14 19:10:29 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import NavBar from './components/NavBar';
 import Hero from './components/Hero';
-import Productviewer from './components/productviewer';
+import Productviewer from './components/Productviewer';
 import gsap from 'gsap';
 import { ScrollTrigger } from 'gsap/all';
 import GameSection from './components/GameSection';
